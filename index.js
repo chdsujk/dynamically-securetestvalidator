6dae2c3c1efa343413703cf13cba43318f049a3a
chdsujk <gs1juanhg18@hotmail.com>
2024-05-15 06:51:11 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function numSquares(n) {
-  const dp = new Array(n + 1).fill(Infinity);
-  dp[0] = 0;
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j * j <= i; j++) {
-      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+function removeDuplicates(nums) {
+  let index = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[index]) {
+      index++;
+      nums[index] = nums[i];
     }
   }
-  return dp[n];
+  return index + 1;
 }
